95fc36fa38ea376d4e6b6623e3b621eeda8ee3d9
jennaniemi <jenna.niemi04@gmail.com>
2025-03-11 09:41:43 +0200

    first commit

---
+++ firebaseConfig.js
@@ -0,0 +1,16 @@
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA10tcJNENV8inlwWct20W3_mgrDd_y1FA",
+  authDomain: "location-app-6b678.firebaseapp.com",
+  projectId: "location-app-6b678",
+  storageBucket: "location-app-6b678.firebasestorage.app",
+  messagingSenderId: "309549242426",
+  appId: "1:309549242426:web:f35ecf96681dba75bf3d03",
+  measurementId: "G-NKGJQN447G"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
